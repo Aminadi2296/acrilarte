49cb087837ccd5781226c7a01d3585f86679228a
Moroni Perez <66393582+Aminadi2296@users.noreply.github.com>
2026-01-12 16:25:14 -0400

    feat: initial structure for hero section add bg image

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,37 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <main className="w-full relative">
+      <section className="relative h-screen w-full flex items-center justify-center text-white overflow-hidden">
+        
+        <div className="absolute inset-0 w-full h-full">
+          <Image
+            src="/laser-machine-bed.jpeg" 
+            alt="Fondo Hero"
+            fill
+            priority
+            className="object-cover -z-20" 
+          />
+
+          <div className="absolute inset-0 bg-black/80 -z-10" />
+        </div>
+
+        <div className="relative z-10 text-center px-6">
+          <p className="text-lg md:text-2xl mt-2 font-light tracking-widest text-gray-300
+          ">
+            Piezas en acrílico que hacen brillar tu espacio
           </p>
+          
+          <button className="mt-8 px-10 py-4 rounded-2xl bg-white/10 backdrop-blur-md border border-white/20 text-white shadow-lg shadow-black/20 hover:bg-white/20 hover:border-white/30 transition-all duration-300 font-bold uppercase tracking-widest">
+            Explorar catálogo
+          </button>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+
+      </section>
+
+
+      <section className="h-screen bg-white">
+      </section>
+    </main>
   );
 }
